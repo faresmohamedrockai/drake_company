7111dd4e3782dfae59957cc85190a43feac95d28
faresmohamedrocka <faresmohamed.rockaidev@gmail.com>
2025-07-21 11:09:10 +0300

    fares moahed 11:09

--- src/contexts/SettingsContext.tsx
+++ src/contexts/SettingsContext.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const defaultSettings = {
   companyName: import.meta.env.VITE_SIDE_BAR_NAME||"company name",
-  companyImage: 'https://www.dpreview.com/files/p/articles/7952219469/google-imagen-lead-image.jpeg',
+  companyImage: import.meta.env.VITE_COMPANY_IMAGE||'https://www.dpreview.com/files/p/articles/7952219469/google-imagen-lead-image.jpeg',
   companyWebsite: import.meta.env.VITE_COMPANY_WEBSITE || 'www.example.com',
   companyEmail:import.meta.env.VITE_COMPANY_EMAIL || 'info@gmail.com' ,
   companyAddress: import.meta.env.VITE_COMPANY_ADDRESS || 'Addrees For Comapny',
